94eec7d7a30704c53dd869886b3c42b43d1264a9
David Nidever <dnidever@lsst.org>
2021-11-04 09:48:00 -0600

    bumped version to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(name='chronos',
 #setup(name='thechronos',
-      version='1.0.0',
+      version='1.0.1',
       description='Automatic isochrone fitting to photometric data',
       author='David Nidever',
       author_email='dnidever@montana.edu',
